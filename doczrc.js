968291509f189e45185a8a6ce14ffa72413c73c1
Alex Lee <alex@alex-lee.site>
2018-09-23 22:43:24 -0700

    Add favicon to doczrc.js

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,6 @@
 export default {
-  ordering: 'ascending'
+  ordering: 'ascending',
+  htmlContext: {
+    favicon: 'https://raw.githubusercontent.com/alexlee-dev/remembera/master/src/assets/favicon/favicon.ico'
+  }
 }
